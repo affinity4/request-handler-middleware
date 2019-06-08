9436d93e13091395cac4ee0a299bf0925d84d709
Luke Watts <luke@luke-watts.com>
2019-06-08 11:52:04 +0100

    Initial commit

---
+++ src/RequestHandlerMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Affinity4\Middleware\RequestHandler;
+
+use Affinity4\MiddlewareStack\Contract\HttpFactoryMiddlewareInterface;
+use Nyholm\Psr7\Factory\Psr17Factory;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class RequestHandlerMiddleware implements HttpFactoryMiddlewareInterface
+{
+    private $HttpFactory;
+
+    public function __construct(Psr17Factory $HttpFactory)
+    {
+        $this->HttpFactory = $HttpFactory;
+    }
+
+    public function process(ServerRequestInterface $Request, RequestHandlerInterface $RequestHandler): ResponseInterface
+    {
+        return $this->HttpFactory->createResponse(200);
+    }
+}
